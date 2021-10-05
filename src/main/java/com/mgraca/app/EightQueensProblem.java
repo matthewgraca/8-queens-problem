4cc97f8d9613d434390b766d62c43cef777715e7
matthewgraca <mgraca@cpp.edu>
2021-10-04 23:43:48 -0700

    renamed app files and added method skeletons

---
+++ src/main/java/com/mgraca/app/EightQueensProblem.java
@@ -0,0 +1,17 @@
+package com.mgraca.app;
+
+public class EightQueensProblem 
+{
+  // if all queens are placed, return true
+  // place queen in first available column
+  // mark places rendered invalid by queens placement
+  // move ot next row
+  public static boolean solve(){
+    return true;
+  }
+
+  public static boolean noCollision(int[][] board){
+    return true;
+  }
+
+}
---
+++ src/test/java/com/mgraca/app/EightQueensProblemTest.java
@@ -0,0 +1,41 @@
+package com.mgraca.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class EightQueensProblemTest 
+{
+  @Test
+  public void test(){
+    assertTrue(true);
+  }
+
+  @Test
+  public void noCollisionsSolution1(){
+    int[][] board = new int[8][8];
+    board[0][3] = 1;
+    board[1][6] = 1;
+    board[2][2] = 1;
+    board[3][7] = 1;
+    board[4][1] = 1;
+    board[5][4] = 1;
+    board[6][0] = 1;
+    board[7][5] = 1;
+    assertTrue(EightQueensProblem.noCollision(board));
+  }
+
+  @Test
+  public void noCollisionsNoSolution(){
+    int[][] board = new int[8][8];
+    board[0][2] = 1;
+    board[1][2] = 1;
+    board[2][2] = 1;
+    board[3][2] = 1;
+    board[4][2] = 1;
+    board[5][5] = 1;
+    board[6][5] = 1;
+    board[7][5] = 1;
+    assertTrue(!EightQueensProblem.noCollision(board));
+  }
+}
